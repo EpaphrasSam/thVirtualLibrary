ed6e9dd90e78392eaa5580a0311130d448940ab0
EpaphrasSam <108902602+EpaphrasSam@users.noreply.github.com>
2022-11-07 23:58:07 +0000

    Update axios.js

--- src/Pages/utils/axios.js
+++ src/Pages/utils/axios.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 
 export default axios.create(
     {
-        baseURL: "https://calm-youtiao-b335fe.netlify.app",
+        baseURL: "thvirtuallibraryserver.netlify.app",
         // baseURL: "http://www.thvirtuallibrary.com:3500",
         // baseURL: "http://localhost:3500",
         withCredentials: true
