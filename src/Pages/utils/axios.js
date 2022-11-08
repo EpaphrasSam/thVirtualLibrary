0774f6a9015173a19dffb3d57d40cabc9f5e6746
EpaphrasSam <108902602+EpaphrasSam@users.noreply.github.com>
2022-11-08 00:08:25 +0000

    Update axios.js

--- src/Pages/utils/axios.js
+++ src/Pages/utils/axios.js
@@ -2,8 +2,8 @@ const axios = require("axios").default;
 
 export default axios.create(
     {
-        // baseURL: "thvirtuallibraryserver.netlify.app",
-        baseURL: "http://www.thvirtuallibrary.com:3500",
+        baseURL: "thvirtuallibraryserver.netlify.app",
+        // baseURL: "http://www.thvirtuallibrary.com:3500",
         // baseURL: "http://localhost:3500",
         withCredentials: true
     });
