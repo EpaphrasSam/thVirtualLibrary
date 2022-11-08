277a44b73a8c1f3b1d17b4e6998ea5fb5d3e4a8f
EpaphrasSam <108902602+EpaphrasSam@users.noreply.github.com>
2022-11-08 08:16:55 +0000

    Update axios.js

--- src/Pages/utils/axios.js
+++ src/Pages/utils/axios.js
@@ -3,6 +3,7 @@ const axios = require("axios").default;
 export default axios.create(
     {
         // baseURL: "http://www.thvirtuallibrary.com:3500",
-        baseURL: "http://localhost:3500",
+        // baseURL: "http://localhost:3500",
+        baseURL: "https://nimble-crostata-422434.netlify.app",
         withCredentials: true
     });
