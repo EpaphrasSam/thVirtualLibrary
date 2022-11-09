f763d34c69adebedcb5ef4917acaeaa6b277a17d
EpaphrasSam <108902602+EpaphrasSam@users.noreply.github.com>
2022-11-09 10:42:42 +0000

    Update axios.js

--- src/Pages/utils/axios.js
+++ src/Pages/utils/axios.js
@@ -4,6 +4,6 @@ export default axios.create(
     {
         // baseURL: "http://www.thvirtuallibrary.com:3500",
         // baseURL: "http://localhost:3500",
-        baseURL: "https://main.d39mrkdlb1glu4.amplifyapp.com",
+        baseURL: "http://ec2-18-212-50-125.compute-1.amazonaws.com:3500",
         withCredentials: true
     });
